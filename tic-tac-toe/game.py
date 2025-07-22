6ef242525290aa4eb830956a3f3cbf66e64968bb
Alive <alivepeterson2@gmail.com>
2025-07-22 15:36:45 +0530

    started code in game.py

---
+++ tic-tac-toe/game.py
@@ -0,0 +1,8 @@
+class TicTacToe:
+    def __init__(self):
+        self.board=[' ' for _ in range (9)] # using a single list, to represent 3x3 matrix shape
+        self.winner=None # to keep track of the winner
+
+    def print_board(self):
+        for row in [self.board[i*3:(i+1)*3] for i in range(3)] :
+            print('| ' + ' | '.join(row) + ' |')
