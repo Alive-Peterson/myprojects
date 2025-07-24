de5d89c1da3dc9b30ee436b143cc0e2f72d5e80d
Alive <alivepeterson2@gmail.com>
2025-07-24 15:03:06 +0530

    Added some code

--- tic-tac-toe/game.py
+++ tic-tac-toe/game.py
@@ -6,3 +6,19 @@ class TicTacToe:
     def print_board(self):
         for row in [self.board[i*3:(i+1)*3] for i in range(3)] :
             print('| ' + ' | '.join(row) + ' |')
+
+    @staticmethod
+    def print_board_nums():
+        # 0 | 1 | 2 it tells us which box responds to which number
+        number_board=[[str(i) for i in range((j*3),(j+1)*3)] for j in range(3)]
+        for row in number_board:
+            print('| ' + ' | '.join(row) + ' |')
+
+    def available_moves(self):
+        return [i for i,spot in enumerate(self.board) if spot==' ']
+        #moves=[]
+        #for (i,spot) in enumerate(self.board):
+        #    if spot==' ':
+        #        moves.append(i)
+        #return moves
+        
