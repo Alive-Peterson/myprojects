d099347b407ee990095f076b3a9126605566164f
Alive <alivepeterson2@gmail.com>
2025-07-24 15:29:41 +0530

    Corrected move attribute into game

--- tic-tac-toe/player.py
+++ tic-tac-toe/player.py
@@ -6,14 +6,14 @@ class Player:
         # letter can be x or o
         self.letter=letter
 
-    def get_move(self,move):
+    def get_move(self,game):
         pass
 
 class RandomComputerPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
 
-    def get_move(self, move):
+    def get_move(self, game):
         square=random.choice(game.available_moves())
         return square
 
@@ -21,5 +21,19 @@ class HumanPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
 
-    def get_move(self, move):
-        pass
+    def get_move(self, game):
+        valid_square=False
+        val=None
+        while not valid_square:
+            square=input(self.letter + '\'s turn. Input move (0-9):')
+            # we are going to check that it's a correct value by trying to cast it as an integer
+            # and if it's not its invalid and if that spot is not available on the board we also say it's invalid
+            try:
+                val=int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square=True #if these are successfull, then good
+            except ValueError:
+                print('Invalid square, Try again')
+            
+            return val
