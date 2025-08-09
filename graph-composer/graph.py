7d313e9a2814968d9ddcdd9a2f782aac72a08207
Alive <alivepeterson2@gmail.com>
2025-08-09 16:07:12 +0530

    Graph Composer

---
+++ graph-composer/graph.py
@@ -0,0 +1,45 @@
+import random
+
+class Vertex(object):
+    def __init__(self, value):
+        self.value = value
+        self.adjacent = {}
+
+    def add_edge_to(self, vertex, weight=0):
+        #adding an edge to the vertex we input as weight
+        self.adjacent[vertex] = weight
+
+    def increment_edge(self, vertex):
+        #incrementing the weight of the edge
+        self.adjacent[vertex] = self.adjacent.get(vertex, 0) + 1
+
+    def get_adjacent_nodes(self):
+        pass
+
+    # initializes probability map
+    def get_probability_map(self):
+        pass
+
+    def next_word(self):
+        pass
+
+
+
+class Graph(object):
+    def __init__(self):
+        pass
+
+    def get_vertex_values(self):
+        pass
+
+    def add_vertex(self, value):
+        pass
+
+    def get_vertex(self, value):
+        pass
+
+    def get_next_word(self, current_vertex):
+        pass
+
+    def generate_probability_mappings(self):
+        pass
