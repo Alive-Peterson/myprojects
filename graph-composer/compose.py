1c19d873e86044f624d62dbe3fa44853f00ac03e
Alive <alivepeterson2@gmail.com>
2025-08-11 16:12:21 +0530

    added get words function

--- graph-composer/compose.py
+++ graph-composer/compose.py
@@ -6,7 +6,13 @@ import random
 from graph import Graph, Vertex
 
 def get_words_from_text(text_path):
-    pass
+    with open (text_path, 'r') as f:
+        text = f.read()
+        text = ' '.join(text.split()) #turns whitespaces into just spaces
+        text = text.lower() #makes everything lower case inorder to compare 
+        text = text.translate(str.maketrans('','', string.punctuation)) # removes all the punctuations
+    words = text.split() # splits on spaces again
+    return words
 
 def make_graph(words):
     pass
