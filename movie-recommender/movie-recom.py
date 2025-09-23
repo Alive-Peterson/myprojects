6844838d4a6a736e4280c8b1b633cdb381accc8a
Alive <alivepeterson2@gmail.com>
2025-09-23 16:05:36 +0530

    Train test, user item matrix

--- movie-recommender/movie-recom.py
+++ movie-recommender/movie-recom.py
@@ -15,3 +15,19 @@ movies = pd.read_csv(MOVIES_FILE)
 print(f"ratings row:", len(ratings))
 print(f"movies row:", len(movies))
 
+#train test split
+from sklearn.model_selection import train_test_split
+train_ratings, test_ratings = train_test_split(ratings, test_size=0.2, random_state=42)
+
+#creating user item matrix(train)
+train_data = pd.merge(train_ratings, movies, on ='movieId', how ='left')
+test_data = pd.merge(test_ratings, movies, on ='movieId', how ='left')
+
+user_movie_train = train_data.pivot_table(index='userId', columns='movieId', values='rating')
+user_movie_train_filled = user_movie_train.fillna(0)  # fill NaN with 0 for KNN
+
+print("Train matrix shape:", user_movie_train.shape)
+
+# Mean Clustering
+user_means = user_movie_train.mean(axis=1)
+normalized = user_movie_train.sub(user_means, axis=0).fillna(0)
